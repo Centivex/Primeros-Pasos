0b635f46cc34d5f4c061a86fe2731e56c53465ca
Abrahams <Abranhamscarnota@gmail.com>
2020-07-26 15:25:53 +0200

    problemas con el import

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -4,22 +4,55 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	private BitmapFont font;
+	//private BitmapFont font;
 
-	//Texture img;
-	
+	//private Texture texPlayer;
+	//private Sprite  spritPlayer;
+
+	private Pixmap pixDibujo;
+	private Texture texDibujo;
+	private Sprite spritDibujo;
+
+
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
-		font = new BitmapFont();
-		font.setColor(Color.BLUE);
+		//dibujar cuadrado y rellaenarlo de ese color
+		pixDibujo = new Pixmap(256,128, Pixmap.Format.RGBA8888);
+		pixDibujo.setColor(Color.GREEN);
+		pixDibujo.fill();
+
+		//dibujar linea
+		pixDibujo.setColor(Color.BLACK);
+		pixDibujo.drawLine(0, 0, pixDibujo.getWidth()-1, pixDibujo.getHeight()-1);
+		pixDibujo.drawLine(0, pixDibujo.getHeight()-1, pixDibujo.getWidth()-1, 0);
+
+		//dibujar u circulo
+		pixDibujo.setColor(Color.YELLOW);
+		pixDibujo.drawCircle(pixDibujo.getWidth()/2, pixDibujo.getHeight()/2, pixDibujo.getHeight()/2 - 1);
+
+
+		texDibujo = new Texture(pixDibujo);
+
+		pixDibujo.dispose();
+
+		spritDibujo= new Sprite(texDibujo);
+
+		//texPlayer= new Texture("Player/caminar_abajo_1.png");
+		//spritPlayer= new Sprite(texPlayer);
+
+		//font = new BitmapFont();
+		//font.setColor(Color.BLUE);
 	}
 
 	@Override
@@ -27,15 +60,23 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
-		font.draw(batch,"HOLA",200,200);
+
+		spritDibujo.setPosition(0, 0);
+		spritDibujo.draw(batch);
+		spritDibujo.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		spritDibujo.draw(batch);
+
+		//spritPlayer.draw(batch);
+
+		//font.draw(batch,"HOLA",200,200);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//img.dispose();
-		font.dispose();
+		texDibujo.dispose();
+		//texPlayer.dispose();
+		//font.dispose();
 	}
 }
