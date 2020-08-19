5b0013bb28692b9bc1d0cf7d087085cc3f343000
Abrahams <Abranhamscarnota@gmail.com>
2020-08-19 16:14:43 +0200

    Sonido y Musica

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -4,20 +4,30 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.backends.lwjgl.audio.Mp3;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.utils.Timer;
 
 
-public class Game extends ApplicationAdapter implements InputProcessor {
+public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 
+	//private Sound sonido;
+	//para guardar algo que tenga grandes datos
+	//private long idSonidoContinua;
+
+	private Music musica;
+
 	private OrthographicCamera cam;
-	private Texture textFondo;
-	private Sprite spritFondo;
+
 
 
 
@@ -32,19 +42,43 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 
 		cam= new OrthographicCamera(1280,720);
 
+		//sonido= Gdx.audio.newSound(Gdx.files.internal("sonido/cancion.mp3"));
+		musica=Gdx.audio.newMusic(Gdx.files.internal("sonido/cancion.mp3"));
+
+		//play
+		//sonido.play();
+
+		//play controlando el sonido
+		//sonido.play(0.5f);
 
 
-		textFondo= new Texture("fondo.jpg");
-		textFondo.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		//en loop cada 5 segundos
 
+		//no sólo puedes meter el loop en long, tambien puedes meter un play,etc
+//		idSonidoContinua = sonido.loop();
+//		Timer.schedule(new Timer.Task(){
+//			@Override
+//			public void run(){
+//				sonido.stop(idSonidoContinua);
+//			}
+//		}, 5.0f);
 
+		//cambiar el volumen con id
+		//sonido.setPitch(idSonidoContinua,0.5f);
 
-		spritFondo= new Sprite(textFondo);
-		spritFondo.setOrigin(0,0);
-		spritFondo.setPosition(-spritFondo.getWidth()/2,-spritFondo.getHeight()/2);
+		//cambiar el volumen y tenerlo en estereo (primer valor oido derecho , segundo el izquierdo)
+		//sonido.setPan(idSonidoContinua, 1,1);
+//-------------------------------------------------------------------------------------------------------------------------
+		//musica
 
+		musica.play();
 
-		Gdx.input.setInputProcessor(this);
+		musica.setVolume(1.0f);
+		musica.pause();
+		musica.stop();
+		musica.play();
+		//te devuelve en que segundo está la cancion
+		Gdx.app.log("SONG",Float.toString(musica.getPosition()));
 
 	}
 
@@ -55,16 +89,10 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 
 		batch.setProjectionMatrix(cam.combined);
 
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			cam.translate(10,0);
-		}else  if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			cam.translate(-10,0);
-		}
 
 
 		cam.update();
 		batch.begin();
-		spritFondo.draw(batch);
 		batch.end();
 	}
 
@@ -74,51 +102,8 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		textFondo.dispose();
-
+		//sonido.dispose();
+		musica.dispose();
 	}
 
-	//------------------------------------------------------------------------------------------------------------------
-
-	//cosas del InputProcessor
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
-	}
 }
